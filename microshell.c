33b31017274c7dc8ce53f405480ad815dab08533
Taichi Tashiro <tatashir@c1r16s7.42tokyo.jp>
2023-10-28 14:11:38 +0900

    fix

--- microshell.c
+++ microshell.c
@@ -12,7 +12,7 @@ int err(char *s)
 
 int cd(char **argv, int i)
 {
-    if (i != 2) //argcが2 つまり $ cd <dir> のように、cdコマンドの引数が一つだけの状態
+    if (i != 2)
         return err("error: cd: bad arguments\n");
     else if (chdir(argv[1]) == -1)
         return err("error: cd: cannot change directory to "), err(argv[1]), err("\n");
@@ -23,20 +23,20 @@ int exec(char **argv, char **envp, int i)
 {
     int fd[2];
     int status;
-    int has_pipe = argv[i] && !strcmp(argv[i], "|");
-    if (has_pipe && pipe(fd) == -1)
+    int hasPipe = argv[i] && !strcmp(argv[i], "|");
+    if (hasPipe && pipe(fd) == -1)
         return err("error: fatal\n");
     int pid = fork();
     if (!pid)
     {
-        argv[i] = 0; //引数配列の終端を示すためにargv[i]に'\0'を代入
-        if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+        argv[i] = 0;
+        if (hasPipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
             return err("error: fatal\n");
         execve(*argv, argv, envp);
         return err("error: cannot execute "), err(*argv), err("\n");
     }
     waitpid(pid, &status, 0);
-    if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+    if (hasPipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
         return err("error: fatal\n");
     return WIFEXITED(status) && WEXITSTATUS(status);
 }
@@ -61,3 +61,5 @@ int main(int argc, char **argv, char **envp)
     }
     return status;
 }
+
+//strcmp 1001
